30d129ef2de1bd155cfde0128304613b839b6060
hyemijeong <62432916+hyemijeong@users.noreply.github.com>
2022-09-29 18:58:29 +0900

    edit design

--- src/views/get_info.vue
+++ src/views/get_info.vue
@@ -3,7 +3,7 @@
         <div class = "prog">
          <div class="progs" id="progressing"></div>
         </div>
-        <p>0/15</p>
+        <p style ="text-align : left;">0/15</p>
     </div>
     
     
@@ -13,30 +13,30 @@
         <p>2022-09-07</p>
 
         <div class = "Q">
-            <div class ="q1">
-                <p>회사의 업종을 입력해주세요.</p>
-                <input type = "search">
-                <input type = "button" value ="확 인">
+            <div class ="q">
+                <p class ="q_text">회사의 업종을 입력해주세요.</p>
+                <input class = "q_input" type = "search" placeholder="제조업">
+                <input class = "q_btn" type = "button" value ="확 인">
             </div>
 
-            <div class ="q2">
-                <p>현재 운영하는 기업체의 수를 입력해주세요.</p>
-                <input type = "text">
+            <div class ="q">
+                <p class ="q_text">현재 운영하는 기업체의 수를 입력해주세요.</p>   
+                <input class ="q_input" type = "text" placeholder="0">
             </div>
 
-            <div class ="q3">
-                <p>현재 기업의 직원 수를 입력해주세요.</p>
-                <input type = "text">
-                <select name="명" id ="selec">
+            <div class ="q">
+                <p class ="q_text">현재 기업의 직원 수를 입력해주세요.</p>
+                <input class = "q_input" type = "text" placeholder="0"/>
+                <select class = "q_btn" name="명" id ="selec">
                     <option value="">천 명</option>
                     <option value="1">백 명</option>
                     <option value="2">십 명</option>
                     <option value="3">명</option>
                 </select>
             </div>
-            <div class ="q4">
-                <p>연간 근무일 수 를 입력해주세요.</p>
-                <input type = "text">
+            <div class ="q">
+                <p class ="q_text">연간 근무일 수를 입력해주세요.</p>
+                <input class = "q_input" type = "text" placeholder="0">
             </div>
         </div>
         
@@ -67,29 +67,56 @@
     }
 
     .context {
-        margin: 100px 50px;
+        position: absolute;
+        left: 30%;
+        top: 20%
+        
     }
     
     h1.title_info {
         font-family: 'B612';
-        font-size: 50px;
+        font-size: 300;
+        font-weight: nomal;
         line-height: 0px;
         color: #3E7519;
     }
 
-    div.q1 {
-        margin: 30px;
+    .Q {
+        margin-top: 100px;
+        margin-left: 30px;
     }
-    div.q2 {
-        margin: 30px;
+
+    .q {
+        position: relative;
+        margin-top: 70px;
     }
-    div.q3 {
-        margin: 30px;
+
+    .q_text {
+        margin-bottom: 12px;
     }
-    div.q4 {
-        margin: 30px;
+        
+    .q_input {
+        width: 500px;
+        height : 30px;
+        background-color: #ECECEC;
+        border: none;
+        border-radius: 7px;
+    }
+
+    .q_btn{
+        margin-left: 10px;
+        width: 70px;
+        height : 30px;
+        background-color: #ECECEC;
+        border: none;
+        border-radius: 7px;
+    }
+    .q_btn:active {
+        background-color: #C1C1C1;
     }
     .btn {
-        margin: 200px 50px;
+        position: relative;
+        top : 800px;
+        left : 50px;
     }
 </style>
