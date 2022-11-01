92bace0a260733ad9fe8fbd6b95a41dd04067206
Hyemi <62432916+hyemijeong@users.noreply.github.com>
2022-11-01 23:19:39 +0900

    .

---
+++ src/components/measure/input1.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class ="body">
+        <div>
+            <button> ◀ </button>
+        </div>
+        <div class="total_carbon">
+            <img src=""/>
+            <div style="color:#223354; font-weight: 600;">총 탄소 배출량</div>
+
+
+        </div>
+    </div>
+</template>
+
+<style>
+.total_carbon{
+    margin: 30px;
+}
+</style>
---
+++ src/views/measureVue/Input1Measure.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="measure-main">
+        <navigation class="navigation"/>
+        <div class="contents">
+            <MeasureHeader/>
+            <Input1/>
+        </div>
+    </div>
+</template>
+
+
+<style>
+body{
+    margin:0;
+}
+.masure-main{
+    display: flex;
+}
+.navigation{
+    position:fixed;
+
+}
+.contents{
+    padding-left: 15vw;
+}
+</style>
+
+
+<script>
+import navigation from "@/components/Navigation.vue"
+import MeasureHeader from "@/components/measure/Header.vue"
+import Input1 from "@/components/measure/input1.vue"
+    export default {
+        name :"measure",
+        components:{
+            navigation,
+            MeasureHeader,
+            Input1
+        }
+    }
+</script>
