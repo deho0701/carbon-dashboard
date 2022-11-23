d962220f4f7e628a6d02c5337cf0bd4e32f2c416
Hyemi <62432916+hyemijeong@users.noreply.github.com>
2022-11-23 13:42:29 +0900

    .

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="measure-main">
+        <navigation class="navigation"/>
+    </div>
+  </template>
+  
+  
+  <style>
+  .navigation{
+    z-index: 1;
+    position:fixed;
+  }
+  </style>
+  
+  
+  <script>
+  import navigation from "@/components/Navigation.vue"
+    export default {
+        name :"insight",
+        components:{
+            navigation,
+        },
+        data() {
+  
+        },
+    }
+  </script>
